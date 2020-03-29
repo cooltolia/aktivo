5d6bf6dd20684f10ebd3cddc938c4ee4f8e63072
Anatoliy Kulak <cooltolia@gmail.com>
2020-03-30 00:35:56 +0300

    added objects-page

--- app/js/main.js
+++ app/js/main.js
@@ -597,6 +597,13 @@ jQuery(document).ready(function ($) {
     })();
 
     (function () {
+      tippy('.object-card__tooltip', {
+        placement: 'bottom',
+        arrow: '<svg xmlns="http://www.w3.org/2000/svg" width="13" height="6" fill="none"><path d="M5.094 1.265a2 2 0 012.723 0L12.911 6H0l5.094-4.735z" fill="#fed63f"/></svg>'
+      });
+    })();
+
+    (function () {
       var partnersWrapper = document.querySelector('.partners-wrapper');
       if (!partnersWrapper) return;
 
