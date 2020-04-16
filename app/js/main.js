8dc0db3002f76f6157d0add805d31105f628fa84
Anatoliy Kulak <cooltolia@gmail.com>
2020-04-16 21:35:19 +0300

    404 + contacts-page

--- app/js/main.js
+++ app/js/main.js
@@ -474,6 +474,27 @@ jQuery(document).ready(function ($) {
     })();
 
     (function () {
+      var contacts = $('.contacts');
+      if (contacts.length === 0) return;
+      ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+          center: [55.749511, 37.537083],
+          zoom: 15,
+          controls: ['zoomControl']
+        });
+        var myPlacemark = new ymaps.Placemark([55.749511, 37.537083], {
+          hintContent: '123112, Москва, Пресненская наб., д. 12, МФК «Федерация Восток»',
+          balloonContent: '123112, Москва, Пресненская наб., д. 12, МФК «Федерация Восток»'
+        }, {
+          preset: 'islands#icon',
+          iconColor: '#fed63f'
+        });
+        myMap.behaviors.disable('scrollZoom');
+        myMap.geoObjects.add(myPlacemark);
+      });
+    })();
+
+    (function () {
       var faq = $('.faq');
       if (faq.length === 0) return;
       var faqLinks = $('.faq__questions-link');
---
+++ develop/blocks/contacts/contacts.js
@@ -0,0 +1,27 @@
+(function () {
+    const contacts = $('.contacts');
+    if (contacts.length === 0) return;
+
+    ymaps.ready(function () {
+        const myMap = new ymaps.Map('map', {
+            center: [55.749511, 37.537083],
+            zoom: 15,
+            controls: ['zoomControl'],
+        });
+
+        const myPlacemark = new ymaps.Placemark(
+            [55.749511, 37.537083],
+            {
+                hintContent: '123112, Москва, Пресненская наб., д. 12, МФК «Федерация Восток»',
+                balloonContent: '123112, Москва, Пресненская наб., д. 12, МФК «Федерация Восток»',
+            },
+            {
+                preset: 'islands#icon',
+                iconColor: '#fed63f',
+            }
+        );
+
+        myMap.behaviors.disable('scrollZoom');
+        myMap.geoObjects.add(myPlacemark);
+    });
+})();
