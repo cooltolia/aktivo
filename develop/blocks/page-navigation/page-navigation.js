90cd913d3cff0358f1fb09fa989012300cd6e32b
Anatoliy Kulak <cooltolia@gmail.com>
2021-06-04 21:26:45 +0300

    page-navigation update

--- develop/blocks/page-navigation/page-navigation.js
+++ develop/blocks/page-navigation/page-navigation.js
@@ -1,8 +1,7 @@
 (function () {
     tippy('.page-navigation [data-tippy-content]', {
         placement: 'bottom',
-        arrow:
-            '<svg xmlns="http://www.w3.org/2000/svg" width="13" height="6" fill="none"><path d="M5.094 1.265a2 2 0 012.723 0L12.911 6H0l5.094-4.735z" fill="#fed63f"/></svg>',
+        arrow: '<svg xmlns="http://www.w3.org/2000/svg" width="13" height="6" fill="none"><path d="M5.094 1.265a2 2 0 012.723 0L12.911 6H0l5.094-4.735z" fill="#fed63f"/></svg>',
     });
 
     const fontObserver = new FontFaceObserver('Montserrat', {
@@ -32,8 +31,12 @@
             controls.forEach((control) => (control.style.display = 'none'));
         } else {
             const currentNavLink = getCurrentLink();
-            navigationScrollWrapper.scrollLeft = currentNavLink.offsetLeft;
-            toggleControlsBtns();
+            if (elIsVisible(currentNavLink)) {
+                toggleControlsBtns();
+            } else {
+                navigationScrollWrapper.scrollLeft = currentNavLink.offsetLeft;
+                toggleControlsBtns();
+            }
         }
 
         const debouncedScrollEvent = debounce(toggleControlsBtns, 100);
