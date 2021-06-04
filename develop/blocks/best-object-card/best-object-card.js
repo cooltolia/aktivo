769acab058fa6f19529c95a7fab522ac5e9583a5
Anatoliy Kulak <cooltolia@gmail.com>
2021-06-04 22:44:46 +0300

    best-objects update

--- develop/blocks/best-object-card/best-object-card.js
+++ develop/blocks/best-object-card/best-object-card.js
@@ -4,20 +4,20 @@
 
     const rateColors = ['#1cb96f', '#fed63f', '#ffa30c'];
 
-    bestObjectCards.each((_, card) => {
-        const rate = parseInt($(card).data('rate'));
-        const rateNode = $(card).find('.best-object-card__rate');
+    // bestObjectCards.each((_, card) => {
+    //     const rate = parseInt($(card).data('rate'));
+    //     const rateNode = $(card).find('.best-object-card__rate');
 
-        const stepHeight = 8;
+    //     const stepHeight = 8;
 
-        if (rate >= 80) {
-            rateNode.css('color', rateColors[0]);
-        } else if (rate < 80 && rate >= 50) {
-            rateNode.css('color', rateColors[1]);
-        } else {
-            rateNode.css('color', rateColors[2]);
-        }
+    //     if (rate >= 80) {
+    //         rateNode.css('color', rateColors[0]);
+    //     } else if (rate < 80 && rate >= 50) {
+    //         rateNode.css('color', rateColors[1]);
+    //     } else {
+    //         rateNode.css('color', rateColors[2]);
+    //     }
 
-        $(card).css('top', `calc(${100 - rate}% + ${stepHeight}px`);
-    });
+    //     $(card).css('top', `calc(${100 - rate}% + ${stepHeight}px`);
+    // });
 })();
